b543511fa8d7e8b72e831f2a9f6aec62bddc1f6c
m-triple-m <triplem656@gmail.com>
2022-09-17 18:31:16 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 import Login from './components/Login';
 import Home  from './components/Home';
+import Customizer from "./components/Customizer";
+import { Suspense } from "react";
 
 
 function App() {
@@ -11,6 +13,12 @@ function App() {
       <BrowserRouter>
       
       <Routes>
+      <Route element={
+        <Suspense fallback={null}>
+          <Customizer />
+        </Suspense>
+      } path="/" />
+
       <Route element={<Login></Login>} path="login" />
       <Route element={<Home></Home>} path="home" />
 
